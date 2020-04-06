6a7c0bb7b640cc234afa019c567eed19769965d1
Matheus Mastrocinque <matheus.m.silva@accenture.com>
2020-04-06 18:40:29 -0300

    Ajuste categorias

--- src/app/componentes/custos.component.ts
+++ src/app/componentes/custos.component.ts
@@ -10,7 +10,32 @@ import { Custos } from '../models/custos.model';
 export class CustosComponent implements OnInit {
     custos: any;
     erro: any;
-    constructor(private getService: GetService) { 
+
+    meses = {
+        1: 'Janeiro',
+        2: 'Fevereiro',
+        3: 'Março',
+        4: 'Abril',
+        5: 'Maio',
+        6: 'Junho',
+        7:  'Julho',
+        8: 'Agosto',
+        9: 'Setembro',
+        10: 'Outubro',
+        11: 'Novembro',
+        12: 'Dezembro'
+    };
+
+    categorias = {
+        1: 'Transporte',
+        2: 'Compras Online',
+        3: 'Saúde e Beleza',
+        4: 'Serviços Automotivos',
+        5: 'Restaurantes',
+        6: 'Super Mercados'
+    }
+
+    constructor(private getService: GetService) {
         this.getter();
     }
 
